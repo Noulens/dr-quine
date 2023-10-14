2101a42a4585b7579b72b5f88d67bc89df9648e3
tnoulens <tnoulens@42.fr>
2023-10-14 03:51:06 -0500

    Grace C

--- C/Grace/Grace_kid.c
+++ C/Grace/Grace_kid.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #define B(x)int main(){FILE *fp = fopen("Grace_kid.c", "w+");fprintf(fp, "#define B(x)"x"\n#define X(s)S(s)\n#define S(s)#s\nB(X(B(x)))");fclose(fp);return(0);}
 #define X(s)S(s)
 #define S(s)#s
