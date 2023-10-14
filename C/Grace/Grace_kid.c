08a2528c67c0579d4c19645203fb4394427a018e
tnoulens <tnoulens@42.fr>
2023-10-14 05:30:45 -0500

    Grace C

--- C/Grace/Grace_kid.c
+++ C/Grace/Grace_kid.c
@@ -1,4 +1,8 @@
-#define B(x)int main(){FILE *fp = fopen("Grace_kid.c", "w+");fprintf(fp, "#define B(x)"x"\n#define X(s)S(s)\n#define S(s)#s\nB(X(B(x)))");fclose(fp);return(0);}
-#define X(s)S(s)
-#define S(s)#s
-B(X(B(x)))
+#include <stdio.h>
+#define B(x) int main(){FILE *fp = fopen("Grace_kid.c", "w+");if (!fp) return (-1);fprintf(fp, "#include <stdio.h>\n#define B(x) "x"\n#define STR(s) STR_HELPER(s)\n#define STR_HELPER(s) #s\n/*\n\tIt is not in the stars to hold our destiny but in ourselves.\n*/\nB(STR(B(x)))");if (fclose(fp) == (-1)) return (-1);return(0);}
+#define STR(s) STR_HELPER(s)
+#define STR_HELPER(s) #s
+/*
+	It is not in the stars to hold our destiny but in ourselves.
+*/
+B(STR(B(x)))
