3dd34b2a9e0c40fe4e094dc2972e1ed294407914
tnoulens <tnoulens@42.fr>
2023-10-16 13:44:02 -0500

    Grace ASM

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-#define B(x) int main(){FILE *fp = fopen("Grace_kid.c", "w+");if (!fp) return (-1);fprintf(fp, "#include <stdio.h>\n#define B(x) "x"\n#define STR(s) STR_HELPER(s)\n#define STR_HELPER(s) #s\n/*\n\tIt is not in the stars to hold our destiny but in ourselves.\n*/\nB(STR(B(x)))");if (fclose(fp) == (-1)) return (-1);return(0);}
+#include <fcntl.h>
+#include <unistd.h>
+#define B(x) int main(){int fd = open("Grace_kid.c", 0100 | 01);if (fd == -1) return (-1);dprintf(fd, "#include <stdio.h>\n#include <fcntl.h>\n#include <unistd.h>\n#define B(x) "x"\n#define STR(s) STR_HELPER(s)\n#define STR_HELPER(s) #s\n/*\n\tIt is not in the stars to hold our destiny but in ourselves.\n*/\nB(STR(B(x)))");if (close(fd) == (-1)) return (-1);return(0);}
 #define STR(s) STR_HELPER(s)
 #define STR_HELPER(s) #s
 /*
 	It is not in the stars to hold our destiny but in ourselves.
 */
 B(STR(B(x)))
+int main()
+{
+	int fd = 0101;
+	return 0;
+}
--- C/Grace/Grace_kid.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#define B(x) int main(){FILE *fp = fopen("Grace_kid.c", "w+");if (!fp) return (-1);fprintf(fp, "#include <stdio.h>\n#define B(x) "x"\n#define STR(s) STR_HELPER(s)\n#define STR_HELPER(s) #s\n/*\n\tIt is not in the stars to hold our destiny but in ourselves.\n*/\nB(STR(B(x)))");if (fclose(fp) == (-1)) return (-1);return(0);}
-#define STR(s) STR_HELPER(s)
-#define STR_HELPER(s) #s
-/*
-	It is not in the stars to hold our destiny but in ourselves.
-*/
-B(STR(B(x)))
