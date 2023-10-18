81c67fa00fd787b8550cff7c6dba3044f33d45e7
tnoulens <tnoulens@42.fr>
2023-10-18 14:15:08 -0500

    Sully C

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,14 +1,59 @@
 section .note.GNU-stack
 section .bss
-i resb 1
+i resq 1
+fd resq 1
 section .data
-source db "", 0
-kiddo db "", 0
+source db "haha", 0
+kiddo db "Sully_%d.c", 0
+cmd db "cat ./Sully_%d.c", 0
+buff times 11 db 0
+make times 16 db 0
 section .text
 global main
 extern sprintf
 extern dprintf
+extern open
+extern close
+extern system
 main:
-mov dword [rel i], 5
-
+mov qword [rel i], 5
+dec qword [rel i]
+cmp qword [rel i], 0
+jl .exit
+xor rax, rax
+lea rdi, [rel buff]
+lea rsi, [rel kiddo]
+mov rdx, [rel i]
+call sprintf wrt ..plt
+mov rax, 2
+lea rdi, [rel buff]
+mov rsi, 0101
+mov rdx, 0777
+syscall
+cmp rax, -1
+je .quit
+mov [rel fd], rax
+xor rax, rax
+mov rdi, [rel fd]
+lea rsi, [rel source]
+call dprintf wrt ..plt
+mov rax, 3
+mov rdi, [rel fd]
+syscall
+cmp rax, -1
+je .quit
+xor rax, rax
+lea rdi, [rel make]
+lea rsi, [rel cmd]
+mov rdx, [rel i]
+call sprintf wrt ..plt
+lea rdi, [rel make]
+call system wrt ..plt
+xor rax, rax
+ret
+.quit:
+mov rax, -1
+ret
+.exit:
+xor rax, rax
 ret
--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -13,11 +13,11 @@ char buff[11];
 memset(buff, 0, 11);
 sprintf(buff, "Sully_%d.c", i);
 if ((fd = open(buff, 0101, 0777)) == -1) return (-1);
-char *source = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$c#include <string.h>%1$c#include <unistd.h>%1$cint main()%1$c{%1$cint i = %2$d;%1$ci--;%1$cif (i >= 0) {%1$cint fd = -1;%1$cchar buff[11];%1$cmemset(buff, 0, 11);%1$csprintf(buff, %3$cSully_%5$cd.c%3$c, i);%1$cif ((fd = open(buff, 0101, 0777)) == -1) return (-1);%1$cchar *source = %3$c%4$s%3$c;%1$cdprintf(fd, source, 10, i, 34, source, 37);%1$cif (close(fd) == -1) return (-1);%1$cchar make[68];%1$csprintf(make, %3$cgcc -Wall -Wextra -Werror -o Sully Sully_%5$cd.c && sleep 1 && ./Sully%3$c, i);%1$csystem(make);}%1$creturn (0);%1$c}";
+char *source = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$c#include <string.h>%1$c#include <unistd.h>%1$cint main()%1$c{%1$cint i = %2$d;%1$ci--;%1$cif (i >= 0) {%1$cint fd = -1;%1$cchar buff[11];%1$cmemset(buff, 0, 11);%1$csprintf(buff, %3$cSully_%5$cd.c%3$c, i);%1$cif ((fd = open(buff, 0101, 0777)) == -1) return (-1);%1$cchar *source = %3$c%4$s%3$c;%1$cdprintf(fd, source, 10, i, 34, source, 37);%1$cif (close(fd) == -1) return (-1);%1$cchar make[68];%1$csprintf(make, %3$cgcc -Wall -Wextra -Werror -o Sully_%5$c1$d Sully_%5$c1$d.c && ./Sully_%5$c1$d%3$c, i);%1$csystem(make);}%1$creturn (0);%1$c}";
 dprintf(fd, source, 10, i, 34, source, 37);
 if (close(fd) == -1) return (-1);
 char make[68];
-sprintf(make, "gcc -Wall -Wextra -Werror -o Sully Sully_%d.c && sleep 1 && ./Sully", i);
+sprintf(make, "gcc -Wall -Wextra -Werror -o Sully_%1$d Sully_%1$d.c && ./Sully_%1$d", i);
 system(make);}
 return (0);
 }
