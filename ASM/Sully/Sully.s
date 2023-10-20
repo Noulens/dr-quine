ad812310acba7c96267a45415de4b8e0458e5d5b
tnoulens <tnoulens@42.fr>
2023-10-20 03:03:15 -0500

    Sully ASM

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,13 +1,13 @@
 section .note.GNU-stack
 section .bss
 section .data
-	source db "haha", 0
-	kiddo db "Sully_%d.c", 0
-	cmd db "gcc -Wall -Werror -Wextra -o Sully_%1$d Sully_%1$d.c", 0
-	buff times 50 db 0
-	make times 50 db 0
-	i dq 5
-	fd dq -1
+source db "section .note.GNU-stack%1$csection .bss%1$csection .data%1$c", 0
+kiddo db "Sully_%d.c", 0
+cmd db "gcc -Wall -Werror -Wextra -o Sully_%1$d Sully_%1$d.c", 0
+buff times 50 db 0
+make times 50 db 0
+i dq 5
+fd dq -1
 section .text
 global main
 extern sprintf
@@ -17,50 +17,48 @@ extern open
 extern close
 extern system
 main:
-	push rbp
-	mov rbp, rsp
-	dec qword [rel i]
-	cmp qword [rel i], 0
-	jl .exit
-	xor rax, rax
-	lea rdi, [rel buff]
-	lea rsi, [rel kiddo]
-	mov rdx, [rel i]
-	call sprintf wrt ..plt
-	mov rax, 2
-	lea rdi, [rel buff]
-	mov rsi, 0101
-	mov rdx, 0777
-	syscall
-	and rax, 0x0f
-	cmp rax, -1
-	je .quit
-	mov [rel fd], rax
-	xor rax, rax
-	mov rdi, [rel fd]
-	lea rsi, [rel source]
-	call dprintf wrt ..plt
-	mov rax, 3
-	mov rdi, [rel fd]
-	syscall
-	cmp rax, -1
-	je .quit
-	xor rax, rax
-	lea rdi, [rel make]
-	lea rsi, [rel cmd]
-	mov rdx, [rel i]
-	call sprintf wrt ..plt
-	lea rdi, [rel make]
-	call system wrt ..plt
-	xor rax, rax
-	mov rsp, rbp
-	pop rbp
+push rbp
+mov rbp, rsp
+dec qword [rel i]
+cmp qword [rel i], 0
+jl .exit
+xor rax, rax
+lea rdi, [rel buff]
+lea rsi, [rel kiddo]
+mov rdx, [rel i]
+call sprintf wrt ..plt
+mov rax, 2
+lea rdi, [rel buff]
+mov rsi, 0101
+mov rdx, 0777
+syscall
+and rax, 0x0f
+cmp rax, -1
+je .quit
+mov [rel fd], rax
+xor rax, rax
+mov rdi, [rel fd]
+lea rsi, [rel source]
+call dprintf wrt ..plt
+mov rax, 3
+mov rdi, [rel fd]
+syscall
+cmp rax, -1
+je .quit
+xor rax, rax
+lea rdi, [rel make]
+lea rsi, [rel cmd]
+mov rdx, [rel i]
+call sprintf wrt ..plt
+lea rdi, [rel make]
+call system wrt ..plt
+xor rax, rax
+mov rsp, rbp
+pop rbp
 ret
-
 .quit:
-    mov rax, -1
+mov rax, -1
 ret
-
 .exit:
-    xor rax, rax
+xor rax, rax
 ret
