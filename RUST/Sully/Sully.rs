845139c04f071ae72debbc6684a97e4440fa1b22
tnoulens <tnoulens@42.fr>
2023-11-02 12:22:12 -0500

    Sully RUST

--- RUST/Sully/Sully.rs
+++ RUST/Sully/Sully.rs
@@ -1,17 +1,24 @@
-// use std::fs::File;
-// use std::io::Write;
+use std::fs::File;
+use std::io::Write;
 use std::process::Command;
 fn main()
 {
-	let output = Command::new("sh").arg("-c").arg("rustc --version").output().expect("Failed to execute command");
-	if output.status.success()
+	let mut i = 5;
+	i -= 1;
+	if i >= 0
 	{
-		let stdout = String::from_utf8_lossy(&output.stdout);
-		print!("{}", stdout);
-	}
-	else
-	{
-		let stderr = String::from_utf8_lossy(&output.stderr);
-		eprint!("Command failed:\n{}", stderr);
+		let mut file = File::create(format!("Sully_{}.rs", i)).expect("Unable to create file");
+		write!(file, "test").expect("Unable to write file");
+		let output = Command::new("sh").arg("-c").arg(format!("echo Sully_{}", i)).output().expect("Failed to execute command");
+		if !output.status.success()
+		{
+			let stderr = String::from_utf8_lossy(&output.stderr);
+			eprint!("Command failed:\n{}", stderr);
+		}
+		else
+		{
+			let stdout = String::from_utf8_lossy(&output.stdout);
+			print!("{}", stdout);
+		}
 	}
 }
